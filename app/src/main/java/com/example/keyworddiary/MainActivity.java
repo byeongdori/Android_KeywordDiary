1bd25a6e45faad0f0b704b18c3992f0d7ae47f05
superbottlegod <superbottlegod@naver.com>
2021-11-30 23:30:24 +0900

    11/30 - 메인 액티비티 데이터 받아서 활용하는 로직 구현

--- app/src/main/java/com/example/keyworddiary/MainActivity.java
+++ app/src/main/java/com/example/keyworddiary/MainActivity.java
@@ -6,17 +6,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 import com.example.keyworddiary.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
     // DB 객체
     public myDBHelper myDB = new myDBHelper(this);
 
+    // 유저 이름 저장하는 변수
     public String Username = null;
 
     @Override
@@ -35,6 +40,26 @@ public class MainActivity extends AppCompatActivity {
             binding.Greetingtext.setText("유저 이름이 설정 되어있지 않아요!");
             binding.SetUsernameText.setText("설정 창에 가서 유저 이름을 설정해주세요!");
         }
+
+        // Seekbar에 리스너 등록, 점수대 별로 변화하는 텍스트 설정하기
+        binding.todayScore.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            int currentscore;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                seekBar.getProgress();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         setContentView(view);
     }
 
@@ -51,9 +76,16 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    // 점수바 동작 감지 함수
-    public int getTodayScore(View view) {
-        // binding.todayScore.
-        return 0;
+    // 사용자가 정보 다 입력한 후, 다음 화면으로 가는 버튼 눌렀을 때 동작하는 함수
+    public void mainactivityResult(View view) {
+
+        // 사용자가 입력한 키워드 변수에 저장하는 작업
+        String works = binding.todayWork.getText().toString();
+        String feelings = binding.todayFeeling.getText().toString();
+        ArrayList<String> todayworks = new ArrayList<>(Arrays.asList(works.replaceAll(" ","").split(",")));
+        ArrayList<String> todayfeelings = new ArrayList<>(Arrays.asList(feelings.replaceAll(" ","").split(",")));
+        int todayScore = binding.todayScore.getProgress();
+
+        // 변수 활용하기, DB에 저장하고 다음으로 넘어가나? 여기가 처리할 부분이 많겠네, 결과 페이지로 넘어갈 때 로딩?
     }
 }
